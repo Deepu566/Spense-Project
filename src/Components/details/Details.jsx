f51f30b15e41bc7722feb3b482d439e4c45c8110
Deepu566 <deepu566255@gmail.com>
2023-01-15 06:22:51 +0530

    map fixed

--- src/Components/details/Details.jsx
+++ src/Components/details/Details.jsx
@@ -21,7 +21,7 @@ const Details = () => {
                         </div>
                     </div>
                     <div className="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.901258548866!2d77.1164787145575!3d28.542687494893592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1c1372346d55%3A0x8cbcbe3054243ad7!2sHotel%20Vishal%20Residency%20by%20RPI!5e0!3m2!1sen!2sin!4v1673538370397!5m2!1sen!2sin" width="400" height="300" style={{ border: '0' }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.901258548866!2d77.1164787145575!3d28.542687494893592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1c1372346d55%3A0x8cbcbe3054243ad7!2sHotel%20Vishal%20Residency%20by%20RPI!5e0!3m2!1sen!2sin!4v1673538370397!5m2!1sen!2sin" width="100%" height="100%" style={{ border: '0' }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
                 <div className="feature">
