744c235eb319c32fddcf1f5fc558e2c0b03eb6c4
Deepu566 <deepu566255@gmail.com>
2023-01-15 05:59:21 +0530

    fixed calender bug

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -18,7 +18,10 @@ const Header = () => {
     const [roomVal, SetRoomVal] = useState(1);
     const [childVal, SetChildVal] = useState(0);
     const [adultVal, SetAdultVal] = useState(1);
-
+    const [isCheckIn, setIsCheckIn] = useState(false)
+    const [isCheckOut, setIsCheckOut] = useState(false)
+    const [checkIn, setCheckIn] = useState('')
+    const [checkOut, setCheckOut] = useState('')
     const submit = () => {
         SetRoomVal(room);
         SetAdultVal(adult);
@@ -26,79 +29,109 @@ const Header = () => {
         setIsFilterOpen(false)
     }
 
-    const [isCheckIn, setIsCheckIn] = useState(false)
-    const [isCheckOut, setIsCheckOut] = useState(false)
 
-    const [checkIn, setCheckIn] = useState('')
-    const [checkOut, setCheckOut] = useState('')
 
     const handleDateChange = (value, type) => {
+        console.log("clicked")
         if (type === 'checkIn') {
             setCheckIn(dateFormatter(value));
             setIsCheckIn(false);
-            console.log('hello')
         }
         if (type === 'checkOut') {
-            console.log('first')
             setCheckOut(dateFormatter(value));
             setIsCheckOut(false);
         }
+    }
+    const handleCalender = (type) => {
+        if (type === "checkIn") {
+            setIsCheckIn(true);
+            isCheckOut && setIsCheckOut(false);
+            setIsFilterOpen(false)
+
+        }
+        if (type === "checkOut") {
+            setIsCheckOut(true);
+            isCheckIn && setIsCheckIn(false)
+            setIsFilterOpen(false)
+        }
+
     }
 
     return (
         <div className='header'>
             <div className="wrap">
                 <div className="menu">
-                    <div className="in" onClick={() => setIsCheckIn(true)}>
+                    <div className="in" onClick={() => handleCalender("checkIn")}>
+                        {
+                            isCheckIn &&
+                            (
+                                <Calendar className="inCalendar cal" onChange={(value) => handleDateChange(value, 'checkIn')} />
+                            )
+                        }
                         {
                             checkIn ? (
                                 <>
                                     <CalendarMonthIcon />
-                                    <label htmlFor="checkin">{checkIn}</label>
-                                    <KeyboardArrowDownIcon />
+                                    <span>{checkIn}</span>
+                                    {
+                                        isCheckIn ?
+                                            <KeyboardArrowUpIcon /> :
+                                            <KeyboardArrowDownIcon />
+                                    }
                                 </>
+
                             ) : (
                                 <>
                                     <CalendarMonthIcon />
-                                    <label htmlFor="checkin">Check In</label>
-                                    <KeyboardArrowDownIcon />
+                                    <span>Check In</span>
+                                    {
+                                        isCheckIn ?
+                                            <KeyboardArrowUpIcon /> :
+                                            <KeyboardArrowDownIcon />
+                                    }
                                 </>
                             )
                         }
-                        {
-                            isCheckIn &&
-                            (
-                                <Calendar className="inCalendar" onChange={(value) => handleDateChange(value, 'checkIn')} />
-                            )
-                        }
                     </div>
-                    <div className="out" onClick={() => setIsCheckOut(true)}>
+                    <div className="out" onClick={() => handleCalender("checkOut")}>
                         {
                             checkOut ? (
                                 <>
                                     <CalendarMonthIcon />
-                                    <label>{checkOut}</label>
-                                    <KeyboardArrowDownIcon />
+                                    <span>{checkOut}</span>
+                                    {
+                                        isCheckOut ?
+                                            <KeyboardArrowUpIcon /> :
+                                            <KeyboardArrowDownIcon />
+                                    }
                                 </>
                             ) : (
                                 <>
                                     <CalendarMonthIcon />
-                                    <label>Check Out</label>
-                                    <KeyboardArrowDownIcon />
+                                    <span>Check Out</span>
+                                    {
+                                        isCheckOut ?
+                                            <KeyboardArrowUpIcon /> :
+                                            <KeyboardArrowDownIcon />
+                                    }
                                 </>
                             )
                         }
                         {
                             isCheckOut &&
                             (
-
-                                <Calendar className="inCalendar" onChange={(value) => handleDateChange(value, 'checkOut')} />
+                                <Calendar className="outCalendar cal" onChange={(value) => handleDateChange(value, 'checkOut')} />
                             )
                         }
                     </div>
 
 
-                    <div className="filter">
+                    <div className="filter"
+                        onClick={() => {
+                            !isFilterOpen ? setIsFilterOpen(true) : setIsFilterOpen(false)
+                            setIsCheckIn(false)
+                            setIsCheckOut(false)
+                        }}>
                         <div className="adults">
                             <span className="adult">{adultVal}</span>adults
                         </div>
@@ -111,54 +144,56 @@ const Header = () => {
                             <span className="room">{roomVal}</span>rooms
                         </div>
                         {!isFilterOpen ?
-                            <KeyboardArrowDownIcon className="icon" onClick={() => setIsFilterOpen(true)} /> :
-                            <KeyboardArrowUpIcon className="icon" onClick={() => setIsFilterOpen(false)} />
+                            <KeyboardArrowDownIcon className="icon" /> :
+                            <KeyboardArrowUpIcon className="icon" />
+                        }
+
+                        {
+                            isFilterOpen &&
+                            <div className="filterMenu">
+                                <h1>Maxium 8 guests per room</h1>
+                                <div className="selectRooms select">
+                                    <div className="left">
+                                        <h2>Rooms</h2>
+                                        <div className='caption'>(Max: 8 total guests/room)</div>
+                                    </div>
+                                    <div className="right">
+                                        <RemoveCircleOutlineIcon className="icon" onClick={() => room !== 1 && setRoom(room - 1)} />
+                                        <span>{room}</span>
+                                        <AddCircleIcon className="icon" onClick={() => setRoom(room + 1)} />
+                                    </div>
+                                </div>
+                                <div className="selectadults select">
+                                    <div className="left">
+                                        <h2>Adults</h2>
+                                        <div className='caption'>(Max: 8 total guests/room)</div>
+                                    </div>
+                                    <div className="right">
+                                        <RemoveCircleOutlineIcon className="icon" onClick={() => adult !== 1 && setAdult(adult - 1)} />
+                                        <span>{adult}</span>
+                                        <AddCircleIcon className="icon" onClick={() => setAdult(adult + 1)} />
+                                    </div>
+                                </div>
+                                <div className="selectchild select">
+                                    <div className="left">
+                                        <h2>Children</h2>
+                                        <div className='caption'>(Max: 8 total guests/room)</div>
+                                    </div>
+                                    <div className="right">
+                                        <RemoveCircleOutlineIcon className="icon" onClick={() => child !== 0 && setChild(child - 1)} />
+                                        <span>{child}</span>
+                                        <AddCircleIcon className="icon" onClick={() => setChild(child + 1)} />
+                                    </div>
+                                </div>
+                                <div className="doneBtn">
+                                    <button className='done' onClick={submit} >Done</button>
+                                </div>
+                            </div>
                         }
                     </div>
                 </div>
                 <button className='btn'>Check Avilability</button>
             </div>
-            {isFilterOpen &&
-                <div className="filterMenu">
-                    <h1>Maxium 8 guests per room</h1>
-                    <div className="selectRooms select">
-                        <div className="left">
-                            <h2>Rooms</h2>
-                            <div className='caption'>(Max: 8 total guests/room)</div>
-                        </div>
-                        <div className="right">
-                            <RemoveCircleOutlineIcon className="icon" onClick={() => room !== 1 && setRoom(room - 1)} />
-                            <span>{room}</span>
-                            <AddCircleIcon className="icon" onClick={() => setRoom(room + 1)} />
-                        </div>
-                    </div>
-                    <div className="selectadults select">
-                        <div className="left">
-                            <h2>Adults</h2>
-                            <div className='caption'>(Max: 8 total guests/room)</div>
-                        </div>
-                        <div className="right">
-                            <RemoveCircleOutlineIcon className="icon" onClick={() => adult !== 1 && setAdult(adult - 1)} />
-                            <span>{adult}</span>
-                            <AddCircleIcon className="icon" onClick={() => setAdult(adult + 1)} />
-                        </div>
-                    </div>
-                    <div className="selectchild select">
-                        <div className="left">
-                            <h2>Children</h2>
-                            <div className='caption'>(Max: 8 total guests/room)</div>
-                        </div>
-                        <div className="right">
-                            <RemoveCircleOutlineIcon className="icon" onClick={() => child !== 0 && setChild(child - 1)} />
-                            <span>{child}</span>
-                            <AddCircleIcon className="icon" onClick={() => setChild(child + 1)} />
-                        </div>
-                    </div>
-                    <div className="doneBtn">
-                        <button className='done' onClick={submit} >Done</button>
-                    </div>
-                </div>
-            }
 
 
         </div >
